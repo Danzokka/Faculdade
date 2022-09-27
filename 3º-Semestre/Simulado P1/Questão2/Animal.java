fcb6876bc2e7451b9dd88f4e500a124d26c17f86
Rafael Dantas <89138596+Danzokka@users.noreply.github.com>
2022-09-26 22:22:28 -0300

    Add files via upload

---
+++ 3º-Semestre/Simulado P1/Questão2/Animal.java
@@ -0,0 +1,12 @@
+public class Animal {
+    String nome;
+    String raca;
+
+    public Animal(String nome){
+        this.nome = nome;
+    }
+
+    public void caminha(){
+        System.out.println("*passos*");
+    }
+}
