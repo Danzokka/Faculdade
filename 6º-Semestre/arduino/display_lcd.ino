d2b2a5bb0641011e50110d38032937672c8517ac
Danzokka <rafaeldantasboeira@gmail.com>
2024-03-18 20:01:42 -0300

    ALPHABET e DISPLAY LCD

---
+++ 6º-Semestre/arduino/display_lcd.ino
@@ -0,0 +1,28 @@
+#include <LiquidCrystal.h>
+
+//Pinos do LCD
+//LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+LiquidCrystal lcd(4, 3, 11, 12, 13, 14);
+
+#define POTENCIOMETRO A0
+
+void setup() {
+  // Seta colunas e linhas do LCD
+  lcd.begin(16, 2);
+}
+
+void loop() {
+  // Le a entrada do potenciometro
+  int potenciometroValue = analogRead(potenciometroValue);
+
+  // Printa o potenciometro no LCD
+  lcd.setCursor(0, 0);
+  lcd.print("Potenciometro: ");
+  lcd.setCursor(0, 1);
+  lcd.print(potenciometroValue);
+
+  // Delay para atualizar o LCD
+  delay(100);
+  
+  Serial.println(potenciometroValue);
+}
