e03abec635200843ca6a20db792b093f9cb1b910
Danzokka <rafaeldantasboeira@gmail.com>
2024-04-08 15:20:26 -0300

    commit infra

--- 6º-Semestre/arduino/teste_infravermelho.ino
+++ 6º-Semestre/arduino/teste_infravermelho.ino
@@ -10,39 +10,37 @@
 
 LiquidCrystal lcd(RS, E, D4, D5, D6, D7);
 
-// Pino onde o receptor IR está conectado
 const int RECV_PIN = 2;
 
 IRrecv irrecv(RECV_PIN);
 decode_results results;
 
-// Função para retornar uma string baseada no valor de entrada
 String decodeIR(decode_results results)
 {
     switch (results.value)
     {
-    case 0xFF6897:
+    case 0xC101E57B:
         return "0";
-    case: 0xFF30CF:
+    case 0x9716BE3F:
         return "1";
-    case: 0x3D9AE3F7:
+    case 0x3D9AE3F7:
         return "2";
-    case: 0x6182021B:
+    case 0x6182021B:
         return "3";
-    case: 0x8C22657B:
+    case 0x8C22657B:
         return "4";
-    case: 0x488F3CBB:
+    case 0xFF38C7:
         return "5";
-    case: 0x449E79F:
+    case 0x449E79F:
         return "6";
-    case: 0xFF42BD:
+    case 0xFF42BD:
         return "7";
-    case: 0x1BC0157B:
+    case 0xFF4AB5:
         return "8";
-    case: 0x3EC3FC1B:
+    case 0xFF52AD:
         return "9";
     default:
-        return "NULL";
+        return "Null";
     }
 }
 
@@ -58,17 +56,17 @@ void loop()
 {
     if (irrecv.decode(&results))
     {
-        // Verifica se o valor recebido é diferente de 25AE7EE0 antes de exibir
         if (results.value != 0x25AE7EE0)
         {
-            String resultString = decodeIR(results); // Chama a função decodeIR e armazena o retorno em resultString
-
-            lcd.setCursor(0, 1);     // Move o cursor para a segunda linha
-            lcd.print(resultString); // Mostra o valor em hexadecimal
-
-            Serial.println(resultString); // Exibe o valor no Serial Monitor
+            String resultString = decodeIR(results);
+            lcd.clear();
+            lcd.setCursor(0, 0);
+            lcd.print("Botao:");
+            lcd.setCursor(0, 1);
+            lcd.print(resultString);
+            Serial.println(resultString);
         }
 
-        irrecv.resume(); // Prepara para receber o próximo valor
+        irrecv.resume();
     }
 }
